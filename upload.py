e878e91444cad56f0e83db61aa9dd5837839c57f
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2024-08-23 04:15:51 +0900

    Update upload.py

--- upload.py
+++ upload.py
@@ -15,7 +15,7 @@ def main():
     for root,dirs, files in os.walk("."):
         dirs.sort()
         if root == ".":
-            for dir in ('.git','github'):
+            for dir in ('.git','.github'):
                 try:
                     dirs.remove(dir)
                 except ValueError:
