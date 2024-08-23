6b0c502ea065b99d101b97e7d03eec03691c32cc
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2024-08-24 00:21:48 +0900

    [Bronze V] Title: 사분면 고르기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,15 @@
+# 사분면 고르기
+
+import sys
+
+x = int(sys.stdin.readline())
+y = int(sys.stdin.readline())
+
+if x > 0 and y > 0:
+    print("1")
+elif x > 0 and y <0:
+    print("4")
+elif x < 0 and y > 0:
+    print("2")
+else:
+    print("3")
