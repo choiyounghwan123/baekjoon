e297996a506d1bb0ee98c080d14767a8171aa533
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2024-11-12 21:47:29 +0900

    [Silver III] Title: 계단 오르기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,18 +1,23 @@
-# 계단 오르기
+# 2579. 계단 오르기
 
-import sys
-
-N = int(sys.stdin.readline())
-stairs = [0] * 301
-
-for i in range(1,N+1):
-    stairs[i] = int(sys.stdin.readline())
+n = int(input())
+stairs = [0]
+for i in range(n):
+    stairs.append(int(input()))
 
 dp = [0] * 301
+if n == 1:
+    print(stairs[1])
+    exit()
+elif n == 2:
+    print(stairs[1] + stairs[2])
+    exit()
 dp[1] = stairs[1]
-dp[2] = stairs[1] + stairs[2]
-dp[3] = max(stairs[1] + stairs[3],stairs[2] + stairs[3])
+dp[2] = stairs[2] + stairs[1]
 
-for i in range(4,N+1):
-    dp[i] = max(dp[i-2] + stairs[i],dp[i-3] + stairs[i-1] + stairs[i])
-print(dp[N])
+
+for i in range(3,n+1):
+    dp[i] = max(dp[i-2] , dp[i-3] +stairs[i-1]) + stairs[i]
+
+
+print(dp[n])
