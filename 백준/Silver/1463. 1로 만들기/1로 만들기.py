c655c58c17bfdc507e1f67de31da9b4fb073e000
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2024-11-12 21:10:08 +0900

    [Silver III] Title: 1로 만들기, Time: 452 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,16 +1,18 @@
-# 1로 만들기
+# 1463. 1로 만들기
 
-import sys
-
-N = int(sys.stdin.readline())
-
+n = int(input())
 dp = [0] * 1000001
+dp[2] = 1
+dp[3] = 1
 
-for i in range(2,N+1):
-    dp[i] = dp[i-1] + 1
-    if i % 3 == 0:
-        dp[i] = min(dp[i],dp[i//3]+1)
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i // 2] + 1)
+for i in range(4, n + 1):
+    if i % 3 == 0 and i % 2 == 0:
+        dp[i] = min(dp[i // 3], dp[i//2], dp[i-1]) + 1
+    elif i % 2 == 0:
+        dp[i] = min(dp[i // 2], dp[i-1]) +1
+    elif i % 3 == 0:
+        dp[i] = min(dp[i // 3] , dp[i-1]) + 1
+    else:
+        dp[i] = dp[i-1] + 1
 
-print(dp[N])
+print(dp[n])
