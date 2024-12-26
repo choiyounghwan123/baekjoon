11f19eb08c00682704a50460800f4251da52e9eb
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2024-12-27 02:47:44 +0900

    [Silver III] Title: N과 M (8), Time: 68 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,22 @@
+# 15657. N과 M (8)
+
+N,M = map(int,input().split())
+nums = sorted(list(map(int,input().split())))
+
+answers = []
+
+def dfs(m,answer,nums):
+    if m == 1:
+        answers.append(answer)
+        return
+
+    for i in range(len(nums)):
+        dfs(m-1,answer + [nums[i]],nums[i:])
+
+for i in range(N):
+    dfs(M,[nums[i]],nums[i:])
+
+for answer in answers:
+    for i in answer:
+        print(i,end=" ")
+    print()
