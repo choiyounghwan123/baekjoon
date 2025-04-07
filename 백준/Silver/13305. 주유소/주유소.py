e3f2bdefa1a265efa0286d5537ab85c7ca291c55
최영환 <117884300+choiyounghwan123@users.noreply.github.com>
2025-04-07 12:39:32 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,20 +1,21 @@
 # 13305. 주유소
+from operator import length_hint
 
 N = int(input())
 length = list(map(int,input().split()))
 costs = list(map(int,input().split()))
-min_ = 1000000
+
+min_ = 1000000000
 result = 0
 a = 0
-for i in range(N-2,-1,-1):
-    if costs[i] >= min_:
-        result += a * min_
+for i in range(N-1):
+    if min_ > costs[i]:
+        result += min_ * a
         min_ = costs[i]
         a = length[i]
     else:
-        min_ = costs[i]
-        a+=length[i]
-if a:
-    result += a * min_
+        a += length[i]
 
-print(result)
+
+print(result + a * min_)
+
